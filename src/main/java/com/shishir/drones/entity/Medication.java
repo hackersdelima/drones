d4d3f699dda61580b8552362ffcc1c6eb0fd8ebc
Shishir Karki <shishir4913120@gmail.com>
2023-07-14 14:55:18 +0545

    FEAT: Add Medication Entity

---
+++ src/main/java/com/shishir/drones/entity/Medication.java
@@ -0,0 +1,22 @@
+package com.shishir.drones.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "medication")
+public class Medication {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "code")
+    private String code;
+    @Column(name = "image_url")
+    private String image;
+}
---
+++ src/main/java/com/shishir/drones/entity/MedicationRepository.java
@@ -0,0 +1,8 @@
+package com.shishir.drones.entity;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MedicationRepository extends JpaRepository<Medication,Integer> {
+}
